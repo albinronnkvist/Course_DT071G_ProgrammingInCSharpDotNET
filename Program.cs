d7fdf8e0de43b9c48cfc23209dccedaacc1cb9fc
albinronnkvist <albin.ronnqvist@hotmail.se>
2022-01-04 21:00:33 +0100

    Configured connection string and registered DbContext

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
+using ForumAPI.Data;
 using ForumAPI.Repositories.PostRepository;
 using ForumAPI.Repositories.UserRepository;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
